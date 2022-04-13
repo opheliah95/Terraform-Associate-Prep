3a0570e2af59aeb94991a8c78538f580ab6d8924
opheliah95 <opheliah95@gmail.com>
2022-04-13 14:09:38 +0100

    formatted tf files

--- workspace/variables.tf
+++ workspace/variables.tf
@@ -1,9 +1,9 @@
 variable "aws_region" {
-    type=string
-    description = "region to be specified"
+  type        = string
+  description = "region to be specified"
 }
 
 variable "instance_type" {
-    type=string
-    description = "type of ec2 instance"
+  type        = string
+  description = "type of ec2 instance"
 }
