2a3c934cf43141c80a09086507db2717e5dd687e
opheliah95 <opheliah95@gmail.com>
2022-04-14 14:10:29 +0100

    fixed workspace duplication error

--- run-terraform.sh
+++ run-terraform.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # current file path for this repo
-current_dir=$PWD/workspace
+workspace="workspace"
+[[ $PWD == *"$workspace"* ]] \
+    && current_dir=$PWD \
+    || current_dir=$PWD/${workspace}
 
 # assign default var file value if not exist already
 terraform_var_file="${1:-aws.tfvars}"
@@ -11,19 +14,19 @@ echo "current directory is ${current_dir}"
 echo "the variable file is ${terraform_var_file}"
 
 # exec terraform apply function in dir with terraform
-function exec_terraform(){
+function terraform_apply(){
     cd ${current_dir}
     terraform apply --var-file=${terraform_var_file} --auto-approve
 }
 
 # exec terraform show function in dir with terraform
-function show_terraform(){
+function terraform_show(){
     cd ${current_dir}
     terraform show
 }
 
 # exec terraform show function in dir with terraform
-function destroy_terraform(){
+function terraform_destroy(){
     cd ${current_dir}
     terraform destroy --var-file=${terraform_var_file} --auto-approve
 }
