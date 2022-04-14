812869dfed5a389d0c25012ec3a1f947f1ef9622
opheliah95 <opheliah95@gmail.com>
2022-04-14 12:16:11 +0100

    current bash for run terraform

---
+++ run-terraform.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# current file path for this repo
+current_dir=$PWD/workspace
+
+# assign default var file value if not exist already
+terraform_var_file= ${[ -z "$1" ] && "aws.tfvars"}
+
+# some debug to test if the resoruces exist
+echo "current directory is ${current_dir}"
+echo "the variable file is ${terraform_var_file}"
+
+# exec terraform apply function in dir with terraform
+function exec_terraform(){
+    cd ${current_dir}
+    terraform apply --var-file=${terraform_var_file} --auto-approve
+}
+
+# exec terraform show function in dir with terraform
+function show_terraform(){
+    cd ${current_dir}
+    terraform show
+}
