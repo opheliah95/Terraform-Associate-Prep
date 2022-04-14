3373ba1f98b10bac0b2cec3062b8c117d458edcb
opheliah95 <opheliah95@gmail.com>
2022-04-14 11:34:08 +0100

    subnets: private and public subnet setup

--- workspace/main.tf
+++ workspace/main.tf
@@ -28,12 +28,24 @@ resource "aws_vpc" "my-vpc" {
 
 # setup private subnets
 resource "aws_subnet" "private_subnets" {
-  for_each   = var.private_subnets
-  vpc_id     = aws_vpc.my-vpc.id
-  cidr_block = cidrsubnet(var.vpc_cidr, var.cidr_newbits, each.value)
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, var.cidr_newbits, each.value)
   availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
   tags = {
-    Name = each.key
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+# set up public subnet
+resource "aws_subnet" "public_subnets" {
+  for_each          = var.public_subnets
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, var.cidr_newbits, each.value + 100)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+  tags = {
+    Name      = each.key
     Terraform = "true"
   }
 }
